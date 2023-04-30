fbf8c47f7e337f3208633ec9108beaa8920ac6d1
SibiPrakash <srivathsan.prakash@gmail.com>
2023-04-30 11:13:51 -0700

    wip: animations for cook and wrapper

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,11 +7,15 @@ public class PlayerMovement : MonoBehaviour
 	public string axisName;
 	public float speed;
 	public float slipperySpeed;
+	public Animator animator;
 
 	private bool isSlippery = false;
+	private bool isWalking = false;
+	private bool hasGun = false;
 
 	private void Update() {
 		transform.position += GetInputValues(true) * (isSlippery ? slipperySpeed : speed) * Time.deltaTime;
+		
 	}
 
 	private Vector3 GetInputValues(bool raw) {
